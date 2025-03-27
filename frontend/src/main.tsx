ac8cafd3465a1099c396ede4841cbe5a0bcb3dd8
Jisu Lee <dlwltn0430@naver.com>
2025-03-27 20:55:37 +0900

    fix: StrictMode 제거

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
 
 import '@/styles/reset.css';
@@ -11,9 +9,7 @@ import './index.css';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
