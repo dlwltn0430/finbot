cfa23bb3aa42c22218e219b300bbc4eeeb2d3d97
Jisu Lee <dlwltn0430@naver.com>
2025-03-28 17:21:39 +0900

    fix: 채팅방 구분을 위해 로컬스토리지에 id도 같이 저장하도록 수정

--- frontend/src/utils/localStorage.ts
+++ frontend/src/utils/localStorage.ts
@@ -1,13 +1,20 @@
 import { ChatMessage } from '@/api/chat';
 
-export const saveChatToLocal = (chat: {
-  title: string;
-  messages: ChatMessage[];
-}) => {
-  const prev = JSON.parse(localStorage.getItem('chatHistory') || '[]');
-  localStorage.setItem('chatHistory', JSON.stringify([...prev, chat]));
+export const saveChatToLocal = (
+  id: string,
+  updatedChat: { title: string; messages: ChatMessage[] }
+) => {
+  const prev = getChatHistory();
+  const next = prev.map((chat) =>
+    chat.id === id ? { ...chat, ...updatedChat } : chat
+  );
+  localStorage.setItem('chatHistory', JSON.stringify(next));
 };
 
-export const getChatHistory = () => {
+export const getChatHistory = (): {
+  id: string;
+  title: string;
+  messages: ChatMessage[];
+}[] => {
   return JSON.parse(localStorage.getItem('chatHistory') || '[]');
 };
