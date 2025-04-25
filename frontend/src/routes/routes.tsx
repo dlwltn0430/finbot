96cde736114c94dc95dd4dd820c1f11da577b5a6
Jisu Lee <dlwltn0430@naver.com>
2025-04-25 17:36:30 +0900

    feat: route path 설정

---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,22 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+
+import { HomePage } from '@/pages/Home';
+
+import { RouterPath } from './path';
+
+const router = createBrowserRouter([
+  {
+    path: RouterPath.HOME,
+    element: <HomePage />,
+  },
+  {
+    path: RouterPath.NOT_FOUND,
+    element: <Navigate to={RouterPath.HOME} />,
+  },
+]);
+
+export const AppRouter = () => <RouterProvider router={router} />;
