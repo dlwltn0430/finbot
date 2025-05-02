97abc737b85b92066ec82257ef5ce6aac8a4a0be
강민석 <95765721+myeolinmalchi@users.noreply.github.com>
2025-05-03 02:08:10 +0900

    feat: 비동기 http 요청 처리 (#1)
    <!--
    Title: <type>([optional scope]): <description>
    
    ## Type
    
    - build - Changes that affect the build system or external dependencies
    (dependencies update)
    - docs - Documentation only changes
    - feat - A new feature
    - fix - A bug fix
    - chore - Changes which does not touch the code
    - refactor - A code change that contains refactor
    - style - Changes that do not affect the meaning of the code
    - test - Adding missing tests or correcting existing tests and also
    changes for our test app
    - perf - A code change that improves performance
    
    See details: https://flank.github.io/flank/pr_titles/
    -->
    
    ## Description
    - 텍스트 임베딩 및 리랭커 서버 요청시 비동기 처리
    - 재시도 데코레이터 추가
    
    ## Details
    - `create_embedding_async()`
    ```python
    @retry_async(delay=3)
    async def create_embedding_async(
        texts: str | List[str],
        session: ClientSession,
        chunking: bool = True,
        truncate: bool = True,
        html: bool = True,
    ) -> EmbedResult | List[EmbedResult]:
    
        body = {
            "inputs": texts,
            "chunking": chunking,
            "truncate": truncate,
            "html": html,
        }
    
        async with session.post(f"{EMBED_URL}/embed", json=body) as res:
            if res.status == 200:
                raw = await res.read()
                return es.validate_json(raw)
    
            raise Exception("텍스트 임베딩에 실패했습니다.")
    ```
    
    - `@retry_async`
    ```python
    def retry_async(times: int = 10, delay: float = 5.0, is_success=lambda _: True):
    
        def decorator(func):
    
            @wraps(func)
            async def wrapped(*args, **kwargs):
                for _ in range(times):
                    try:
                        result = await func(*args, **kwargs)
                        if not is_success(result):
                            continue
                        if isinstance(result, BaseException):
                            raise result
                        return result
                    except Exception as e:
                        logger(f"요청을 재시도 합니다: {e}", level=logging.WARNING)
                        await asyncio.sleep(delay)
                raise TimeoutError(f'{times}번의 재시도에 실패했습니다.')
    
            return wrapped
    
        return decorator
    ```

--- backend/utils/embed.py
+++ backend/utils/embed.py
@@ -1,12 +1,14 @@
 from typing import List, overload
 
+from aiohttp import ClientSession
 from dotenv import load_dotenv
 import os
 
 from pydantic import TypeAdapter
 import requests
 
-from schemas.embed import EmbedResult
+from common.asyncio import retry_async, retry_sync
+from schemas.embed import EmbedResult, RerankResult
 
 load_dotenv()
 
@@ -16,7 +18,7 @@ es = TypeAdapter(List[EmbedResult] | EmbedResult)
 
 
 @overload
-def embed(
+def create_embedding(
     texts: str,
     chunking: bool = True,
     truncate: bool = True,
@@ -26,7 +28,7 @@ def embed(
 
 
 @overload
-def embed(
+def create_embedding(
     texts: List[str],
     chunking: bool = True,
     truncate: bool = True,
@@ -35,12 +37,14 @@ def embed(
     pass
 
 
-def embed(
+@retry_sync(delay=3)
+def create_embedding(
     texts: str | List[str],
     chunking: bool = True,
     truncate: bool = True,
     html: bool = False,
 ) -> EmbedResult | List[EmbedResult]:
+
     body = {
         "inputs": texts,
         "chunking": chunking,
@@ -53,4 +57,57 @@ def embed(
         raw = res.content
         return es.validate_json(raw)
 
-    raise Exception
+    raise Exception("텍스트 임베딩에 실패했습니다.")
+
+
+@retry_async(delay=3)
+async def create_embedding_async(
+    texts: str | List[str],
+    session: ClientSession,
+    chunking: bool = True,
+    truncate: bool = True,
+    html: bool = True,
+) -> EmbedResult | List[EmbedResult]:
+
+    body = {
+        "inputs": texts,
+        "chunking": chunking,
+        "truncate": truncate,
+        "html": html,
+    }
+
+    async with session.post(f"{EMBED_URL}/embed", json=body) as res:
+        if res.status == 200:
+            raw = await res.read()
+            return es.validate_json(raw)
+
+        raise Exception("텍스트 임베딩에 실패했습니다.")
+
+
+rs = TypeAdapter(List[RerankResult])
+
+
+@retry_async(delay=3)
+async def rerank_async(
+    query: str,
+    texts: List[str],
+    session: ClientSession,
+    truncate: bool = True,
+    truncation_direction="Right",
+) -> List[RerankResult]:
+    if not texts:
+        return []
+
+    body = {
+        "query": query,
+        "texts": texts,
+        "truncate": truncate,
+        "truncation_direction": truncation_direction,
+    }
+
+    async with session.post(f"{EMBED_URL}/rerank", json=body) as res:
+        if res.status == 200:
+            raw = await res.read()
+            return rs.validate_json(raw)
+
+        raise Exception("Failed Reranking")
