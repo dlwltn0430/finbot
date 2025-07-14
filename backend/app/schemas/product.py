6a52bc344b531410ddbe6cbd55606c99b3da44c8
Jisu Lee <dlwltn0430@naver.com>
2025-07-14 11:16:54 +0900

    feat: SSE 응답 처리 (#4)
    * remove: 불필요한 디렉토리 삭제
    
    * feat: route path 설정
    
    * refactor: `localStorage.ts`에서 `chatStorage.ts`로 파일명 변경
    
    * refactor: 사이드바 파일 분리
    
    * refactor: 챗봇 응답 컴포넌트 파일명 변경
    
    * refactor: 메인 채팅 영역(입력 필드 제외) 파일 분리
    
    * refactor: 메인 채팅 영역의 입력 필드 파일 분리
    
    * refactor: `useChat` 훅 생성
    
    * refactor: 홈화면 생성
    
    * refactor: 인터페이스 이름 변경
    
    * refactor: 의존성 배열 추가
    
    * chore: 이전 변경사항 반영
    
    * design: 사이드바 새 디자인 적용
    
    * chore: favicon 변경
    
    * chore: 이미지 파일 폴더 구조 변경
    
    * design: 사이드바 디자인 수정
    
    * design: 이용자 프로필 영역 추가
    
    * design: 입력창 width 조정
    
    * design: 캐러셀 구현
    
    * design: 캐러셀 네비게이션, 페이지네이션 커스텀
    
    * design: `reset.css` 삭제
    
    * design: 하단 이유 설명 부분 퍼블리싱
    
    * chore: prettier-plugin-tailwindcss 설치
    
    * feat: 스피너 컴포넌트 구현
    
    * feat: 인스턴스 생성
    
    * feat: 대화 요청 함수 구현
    
    * chore: 필드명 `company` -> `institution`으로 변경
    
    * feat: 대화 요청 응답을 실제 응답으로 변경
    
    * feat: 채팅, 상품 관련 스키마 정의
    
    * fix: `details` 마크다운으로 렌더링하도록 수정
    
    * feat: JWT 인증 미들웨어 구현
    
    * feat: 의존성 추가
    
    * feat: `Dockerfile` 생성, uv환경 대응
    
    * feat: 버전별 엔드포인트 분리 및 JWT 인증 추가
    
    * feat: 채팅 응답 Mock API 추가
    
    * chore: SSE 응답 테스트 위한 코드 주석 처리
    
    * fix: 빌드 시 발생하는 에러 수정
    
    * feat: 프록시 주소 추가
    
    * feat: API 호출시 절대경로 대신 상대경로 사용
    
    * feat: 의존성
    
    * fix: CORS 오류 해결
    
    OPTIONS 메서드에 대해서는 Auth 생략 필요
    
    * feat: Dockerfile 수정
    
    * feat: SSE 응답 처리
    
    ---------
    
    Co-authored-by: myeolinmalchi <rkd2274@pusan.ac.kr>

---
+++ backend/app/schemas/product.py
@@ -0,0 +1,26 @@
+from typing import List, Literal, Optional
+from pydantic import BaseModel
+
+
+class ProductOptionDTO(BaseModel):
+    category: str
+    value: str
+
+
+class ProductInfoDTO(BaseModel):
+    product_type: Literal["saving", "deposit", "card", "insurance"]
+    description: str
+    institution: str
+    options: List[ProductOptionDTO]
+    tags: List[str]
+    details: str
+
+
+class PartialProductInfoDTO(BaseModel):
+    product_type: Literal["saving", "deposit", "card", "insurance"]
+    description: Optional[str] = None
+    institution: str
+
+    options: Optional[List[ProductOptionDTO]] = None
+    tags: Optional[List[str]] = None
+    details: Optional[str] = None
