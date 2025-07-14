7607d1ee56548bb5b7733547c650eafdf45bc8e6
Jisu Lee <dlwltn0430@naver.com>
2025-07-14 12:35:28 +0900

    feat: 전체 대화 목록 조회 기능 연결

--- frontend/src/utils/chatStorage.ts
+++ frontend/src/utils/chatStorage.ts
@@ -1,34 +1,42 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 import { ChatMessage } from '@/api/chat';
 
+// TODO:
 export interface ChatHistoryItem {
-  id: string;
+  chat_id: string;
   title: string;
   messages: ChatMessage[];
   createdAt: string;
 }
 
-export const saveChatToLocal = (
-  id: string,
-  updatedChat: { title: string; messages: ChatMessage[] }
-) => {
-  const prev = getChatHistory();
-  const next = prev.map((chat) =>
-    chat.id === id ? { ...chat, ...updatedChat } : chat
-  );
-  localStorage.setItem('chatHistory', JSON.stringify(next));
-};
+// TODO: 인터페이스 이름 변경 필요, 추후 로직 수정 필요
+export interface ChatSidebarItem {
+  chat_id: string; // TODO: 컨벤션대로 chatId로 작성하면 배열 돌면서 다 바꿔줘야 하는 문제 있음 -> chat_id로 임시 사용
+  title: string;
+  createdAt: string;
+}
 
-export const getChatHistory = (): ChatHistoryItem[] => {
-  const raw = JSON.parse(
-    localStorage.getItem('chatHistory') || '[]'
-  ) as Partial<ChatHistoryItem>[];
+// export const saveChatToLocal = (
+//   id: string,
+//   updatedChat: { title: string; messages: ChatMessage[] }
+// ) => {
+//   const prev = getChatHistory();
+//   const next = prev.map((chat) =>
+//     chat.id === id ? { ...chat, ...updatedChat } : chat
+//   );
+//   localStorage.setItem('chatHistory', JSON.stringify(next));
+// };
 
-  return raw.map((chat) => ({
-    id: chat.id ?? uuidv4(),
-    title: chat.title ?? '',
-    messages: chat.messages ?? [],
-    createdAt: chat.createdAt || new Date().toISOString(),
-  }));
-};
+// export const getChatHistory = (): ChatHistoryItem[] => {
+//   const raw = JSON.parse(
+//     localStorage.getItem('chatHistory') || '[]'
+//   ) as Partial<ChatHistoryItem>[];
+
+//   return raw.map((chat) => ({
+//     id: chat.id ?? uuidv4(),
+//     title: chat.title ?? '',
+//     messages: chat.messages ?? [],
+//     createdAt: chat.createdAt || new Date().toISOString(),
+//   }));
+// };
