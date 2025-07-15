f2f1501ceb7652d4061ac57a75e70792d9154779
Jisu Lee <dlwltn0430@naver.com>
2025-07-15 19:05:54 +0900

    design: 은행 이미지 매핑

--- frontend/src/constants/institutionImages.ts
+++ frontend/src/constants/institutionImages.ts
@@ -1,23 +1,40 @@
+import busan from '@/assets/bank/busan.svg';
+import cu from '@/assets/bank/cu.svg';
+import ibk from '@/assets/bank/ibk.svg';
+import jeju from '@/assets/bank/jeju.svg';
+import kakao from '@/assets/bank/kakao.svg';
 import kb from '@/assets/bank/kb.svg';
+import kbank from '@/assets/bank/kbank.svg';
+import kdb from '@/assets/bank/kdb.svg';
+import keb from '@/assets/bank/keb.svg';
+import kj from '@/assets/bank/kj.svg';
+import kn from '@/assets/bank/kn.svg';
+import koreapost from '@/assets/bank/koreapost.svg';
+import nh from '@/assets/bank/nh.svg';
+import sc from '@/assets/bank/sc.svg';
+import sh from '@/assets/bank/sh.svg';
+import shinhan from '@/assets/bank/shinhan.svg';
+import toss from '@/assets/bank/toss.svg';
+import woori from '@/assets/bank/woori.svg';
 
 export const institutionImages: Record<string, string> = {
   default: kb,
   KB국민은행: kb,
-  카카오뱅크: kb,
-  부산은행: kb,
-  경남은행: kb,
-  신한은행: kb,
-  하나은행: kb,
-  우리은행: kb,
-  NH농협은행: kb,
-  기업은행: kb,
-  토스뱅크: kb,
-  산업은행: kb,
-  신협: kb,
-  SC제일은행: kb,
-  수협: kb,
-  케이뱅크: kb,
-  광주은행: kb,
-  제주은행: kb,
-  우정사업본부: kb,
+  카카오뱅크: kakao,
+  부산은행: busan,
+  경남은행: kn,
+  신한은행: shinhan,
+  하나은행: keb,
+  우리은행: woori,
+  NH농협은행: nh,
+  IBK기업은행: ibk,
+  토스뱅크: toss,
+  KDB산업은행: kdb,
+  신협: cu,
+  SC제일은행: sc,
+  SH수협은행: sh,
+  케이뱅크: kbank,
+  광주은행: kj,
+  제주은행: jeju,
+  우정사업본부: koreapost,
 };
