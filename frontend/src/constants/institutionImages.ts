8669bdbb852c4f6fa7311088d6699860cfe4eeea
Jisu Lee <dlwltn0430@naver.com>
2025-07-15 11:43:55 +0900

    feat: 은행 이미지 매핑 객체 정의

---
+++ frontend/src/constants/institutionImages.ts
@@ -0,0 +1,23 @@
+import kb from '@/assets/bank/kb.svg';
+
+export const institutionImages: Record<string, string> = {
+  default: kb,
+  KB국민은행: kb,
+  카카오뱅크: kb,
+  부산은행: kb,
+  경남은행: kb,
+  신한은행: kb,
+  하나은행: kb,
+  우리은행: kb,
+  NH농협은행: kb,
+  기업은행: kb,
+  토스뱅크: kb,
+  산업은행: kb,
+  신협: kb,
+  SC제일은행: kb,
+  수협: kb,
+  케이뱅크: kb,
+  광주은행: kb,
+  제주은행: kb,
+  우정사업본부: kb,
+};
