6853742fa7cfa3f34a7c2d62953b4b6d80123939
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-17 10:48:01 +0900

    feat: 입력창 크기 조절

--- frontend/src/components/ChatInput.tsx
+++ frontend/src/components/ChatInput.tsx
@@ -46,7 +46,7 @@ export const ChatInput = ({
   };
 
   return (
-    <div className="flex absolute translate-x-[-50%] left-[calc(50%+76px/2-4px)] bottom-[40px] w-[800px] h-fit">
+    <div className="flex absolute translate-x-[-50%] left-[calc(50%+76px/2-4px)] bottom-[40px] w-[720px] h-fit">
       <textarea
         ref={textareaRef}
         value={input}
