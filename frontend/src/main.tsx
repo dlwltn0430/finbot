3cf713c9e945126d9e203004d26b8b25c514c25f
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-17 10:49:51 +0900

    feat: `styles/reset.css` 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './styles/index.css';
+import './styles/reset.css';
 import { AppRouter } from './routes/routes.tsx';
 
 const queryClient = new QueryClient();
