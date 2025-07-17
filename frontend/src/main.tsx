e360f21478501c4db66aea996d94ce7e6d58208f
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-17 10:49:34 +0900

    refactor: `index.css` -> `styles/index.css`

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import './index.css';
+import './styles/index.css';
 import { AppRouter } from './routes/routes.tsx';
 
 const queryClient = new QueryClient();
