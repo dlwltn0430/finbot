994552ac0e9af2d6bd6add17faac5b7669f0fef1
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-07-27 04:25:18 +0900

    feat: implement new jwt verification middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from langchain_upstage import ChatUpstage
 
 from app.db import init_db
@@ -14,6 +15,7 @@ from fastapi.security.api_key import APIKeyHeader
 
 from app.api.v1 import router as v1_router
 from domains.common.agents.supervisor import init_graph
+from domains.user.auth import TokenService
 
 
 def create_app(lifespan):
@@ -60,3 +62,37 @@ async def lifespan(app: FastAPI):
 
 
 app = create_app(lifespan)
+
+token_service = TokenService(app.state.db)
+
+
+@app.middleware("http")
+async def verify_token_middleware(req: Request, call_next):
+    """토큰 인증 미들웨어"""
+
+    excluded_paths = [
+        "/docs",
+        "/openapi.json",
+        "/api/v1/auth/kakao/callback",
+        "/api/v1/auth/kakao/login",
+    ]
+
+    if req.method == "OPTIONS" or req.url.path in excluded_paths:
+        response = await call_next(req)
+        return response
+
+    access_token = req.cookies.get("access_token")
+    if not access_token:
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED,
+                            content={"detail": "Not authenticated"})
+
+    try:
+        payload = token_service.verify_and_decode_token(access_token)
+        req.state.user_id = payload.get("sub")
+
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED,
+                            content={"detail": "Token has expired or is invalid"})
+
+    response = await call_next(req)
+    return response
