13a9cc2906738605cd8438baadf864525d6f02b2
Jisu Lee <dlwltn0430@naver.com>
2025-08-07 14:54:24 +0900

    design: 스크롤바 위치 조정

--- frontend/src/components/MemoryPanel.tsx
+++ frontend/src/components/MemoryPanel.tsx
@@ -25,27 +25,27 @@ export const MemoryPanel = ({ onClose }: MemoryPanelProps) => {
         </div>
       </div>
 
-      <div className="custom-scroll border-gray2pb-[36px] relative max-h-[80vh] overflow-y-auto rounded-[24px] border p-[36px] shadow-[0_0_12px_0_rgba(27,27,27,0.04)]">
-        {/* <div className="custom-scroll inset-0 overflow-y-auto px-[36px] pb-[36px] pr-[44px] pt-[36px]"> */}
-        <div className="w-[calc(100%-8px)]">
-          {[...Array(10)].map((_, idx, arr) => (
-            <div
-              key={idx}
-              className={`flex items-center justify-between border-b border-gray2 ${idx === 0 ? 'pb-[12px]' : idx === arr.length - 1 ? 'pt-[12px]' : 'py-[12px]'}`}
-            >
-              <span className="text-[12px] font-[400] leading-[16px] text-black">
-                메모리 내용입니다. 내용은 최대 n줄까지 들어갑니다. <br />
-                메모리 내용입니다. 내용은 최대 n줄까지 들어갑니다.
-              </span>
-              <img
-                src={trash}
-                alt="삭제"
-                className="h-[24px] w-[24px] cursor-pointer"
-              />
-            </div>
-          ))}
+      <div className="relative max-h-[80vh] rounded-[24px] border border-gray2 pb-[36px] pl-[36px] pr-[24px] pt-[36px] shadow-[0_0_12px_0_rgba(27,27,27,0.04)]">
+        <div className="custom-scroll max-h-[60vh] overflow-y-auto">
+          <div className="min-w-[calc(100%-8px) mr-[8px]">
+            {[...Array(10)].map((_, idx, arr) => (
+              <div
+                key={idx}
+                className={`flex items-center justify-between border-b border-gray2 ${idx === 0 ? 'pb-[12px]' : idx === arr.length - 1 ? 'pt-[12px]' : 'py-[12px]'}`}
+              >
+                <span className="text-[12px] font-[400] leading-[16px] text-black">
+                  메모리 내용입니다. 내용은 최대 n줄까지 들어갑니다. <br />
+                  메모리 내용입니다. 내용은 최대 n줄까지 들어갑니다.
+                </span>
+                <img
+                  src={trash}
+                  alt="삭제"
+                  className="h-[24px] w-[24px] cursor-pointer"
+                />
+              </div>
+            ))}
+          </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
