dfc27ea01d5ff5df918b208eb962fe9307507036
Jisu Lee <dlwltn0430@naver.com>
2025-08-13 00:03:34 +0900

    design: 정책 피드 퍼블리싱 (#25)
    * feat: 공통 레이아웃 분리
    
    * design: 정책 피드 화면 퍼블리싱
    
    * design: `description` 길이 상관없이 버튼 하단 고정

--- frontend/src/routes/path.ts
+++ frontend/src/routes/path.ts
@@ -2,5 +2,6 @@ export const RouterPath = {
   ROOT: '/',
   HOME: '/',
   CHAT_DETAIL: '/chat/:chatId',
+  FEED: '/feed',
   NOT_FOUND: '*',
 };
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -4,18 +4,31 @@ import {
   createBrowserRouter,
 } from 'react-router-dom';
 
+import { AppLayout } from '@/layouts/AppLayout';
+
+import { FeedPage } from '@/pages/Feed';
 import { HomePage } from '@/pages/Home';
 
 import { RouterPath } from './path';
 
 const router = createBrowserRouter([
   {
-    path: RouterPath.HOME,
-    element: <HomePage />,
-  },
-  {
-    path: RouterPath.CHAT_DETAIL,
-    element: <HomePage />,
+    path: RouterPath.ROOT,
+    element: <AppLayout />,
+    children: [
+      {
+        path: RouterPath.HOME,
+        element: <HomePage />,
+      },
+      {
+        path: RouterPath.CHAT_DETAIL,
+        element: <HomePage />,
+      },
+      {
+        path: RouterPath.FEED,
+        element: <FeedPage />,
+      },
+    ],
   },
   {
     path: RouterPath.NOT_FOUND,
